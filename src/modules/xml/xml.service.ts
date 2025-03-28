e3656fff3584aed8cb959c789d95f56ff1ed1451
dangnh1 <dangnh1.bhsoft@gmail.com>
2025-03-28 16:57:08 +0700

    Update

--- src/modules/xml/xml.service.ts
+++ src/modules/xml/xml.service.ts
@@ -4,11 +4,44 @@ import { XMLParser } from 'fast-xml-parser';
 @Injectable()
 export class XmlService {
     async parseXml(xmlData: string): Promise<any> {
+        xmlData = xmlData.replaceAll('AttributeID="asset.', 'AttributeID="att_');
         const parser = new XMLParser({
             ignoreAttributes: false,
             attributeNamePrefix: '',
-            textNodeName: 'text',
+            textNodeName: 'DamText',
         });
-        return parser.parse(xmlData);
+        const parsedData = parser.parse(xmlData);
+        const assetsArray = parsedData['STEP-ProductInformation']['Assets']['Asset'];
+        return {
+            success: true,
+            data: this.transformAssets(assetsArray),
+        };
+    }
+
+    private transformAssets(assetArray: any[]): any[] {
+        return assetArray.map((asset) => {
+            const initParams = {
+                _id: asset['ID'] || '',
+                name: asset['Name']?.text || '',
+                objectTypeID: asset['UserTypeID'] || '',
+            };
+            if (asset?.['Values']?.['Value']?.length > 0) {
+                asset['Values']['Value'].map((value) => {
+                    initParams[value['AttributeID']] = value['DamText'] || '';
+                });
+            }
+            if (asset?.['Values']?.['ValueGroup']?.length > 0) {
+                asset['Values']['ValueGroup'].map((group) => {
+                    if (group?.['Value']?.length > 0) {
+                        initParams[group['AttributeID']] = group['Value'].map((value) => {
+                            return {
+                                [value['DerivedContextID']]: value['DamText'] || '',
+                            };
+                        });
+                    }
+                });
+            }
+            return initParams;
+        });
     }
 }
